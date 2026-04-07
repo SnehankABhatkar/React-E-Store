989f016dc4249c6156a5c5d4362d04ce58e30fb0
Snehank <snehankab@gmail.com>
2026-04-07 16:35:46 +0530

    Added routing

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -7,6 +7,7 @@ import mkcert from "vite-plugin-mkcert";
 export default defineConfig({
   server: {
     port: 3000,
+    open: true,
   },
   plugins: [react(), mkcert(), babel({ presets: [reactCompilerPreset()] })],
 });
